ddb6295e507cd25ff7dc491d8fe9ed13b7a157be
Hope Giometti <hopegiometti@gmail.com>
2019-09-27 22:53:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute_name, attributes|
+    attributes.each do |attribute_value, pigeon_names|
+      pigeon_names.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+
+        if !pigeon_list[name][attribute_name]
+          pigeon_list[name][attribute_name] = []
+        end
+
+        if !pigeon_list[name][attribute_name].include?(attribute_value)
+          pigeon_list[name][attribute_name] << attribute_value.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
