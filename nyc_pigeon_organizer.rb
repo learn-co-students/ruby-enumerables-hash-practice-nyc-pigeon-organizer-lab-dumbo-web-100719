93a0b7d698f712c1faede9250f71c0381971f12f
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-30 11:43:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  sorted_hash = Hash.new 
+    data.each do |key, value|
+    sorted_hash << data[key]
 end
+  sorted_hash
+end
+
+
+if structure.has_key?(:a) && structure[:a].has_key?(:b) then
+  value = structure[:a][:b]
