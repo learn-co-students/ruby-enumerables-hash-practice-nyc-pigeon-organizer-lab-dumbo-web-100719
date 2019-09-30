cba94e664a52a8d73758a6941da1112ed8ccc8aa
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-30 14:33:42 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,32 @@
 require 'pry'
-def nyc_pigeon_organizer(data)
-#create new hash
-  sorted_hash = {}
-#iterate through original hash
+# def nyc_pigeon_organizer(data)
+# #create new hash
+#   sorted_hash = {}
+# #iterate through original hash
  
-  sorted_hash = data.select {|key, value|
- binding.pry
-  [:b, :d, :e, :f].include?(key) }
-#extract names (need help on this - how to just grab all bottom level values?)
-     # if data.has_value?
-# put names in new hash
-       # sorted_hash = key
+#   sorted_hash = data.select {|key, value|
+# binding.pry
+#   [:b, :d, :e, :f].include?(key) }
+# #extract names (need help on this - how to just grab all bottom level values?)
+#     # if data.has_value?
+# # put names in new hash
+#       # sorted_hash = key
    
-  sorted_hash
-end
+#   sorted_hash
+# end
 
+def nyc_pigeon_organizer(data)
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+       new_hash[name]= new_value
+        binding.pry
+      end
+    end
+  end
+  new_hash
+end
 
 # if structure.has_key?(:a) && structure[:a].has_key?(:b) then
 #   value = structure[:a][:b]
