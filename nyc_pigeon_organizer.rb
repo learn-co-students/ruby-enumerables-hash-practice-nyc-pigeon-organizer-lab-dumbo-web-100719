8c0df05fc5a65aa626ab307c2fc8ea6fead24dac
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-30 15:18:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,5 @@
 require 'pry'
 
-
-# def nyc_pigeon_organizer(data)
-#   new_hash = {}
-#   data.each do |key, value|
-#     value.each do |new_value, names|
-#       names.each do |name|
-#         binding.pry
-#       new_hash[name]= key[new_value]
-
-#       end
-#     end
-#   end
-#   new_hash
-# end
-
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, value|
@@ -31,12 +16,6 @@ def nyc_pigeon_organizer(data)
   binding.pry
 end
 
-# if structure.has_key?(:a) && structure[:a].has_key?(:b) then
-#   value = structure[:a][:b]
-
-#h1 = {:a => :A, :b => :B, :c => :C, :d => :D}
-# h2 = h1.select {|key, value| [:b, :d, :e, :f].include?(key) } # => {:b=>:B, :d=>:D} 
-# h1 = Hash[h1.to_a - h2.to_a]
 
 
 
