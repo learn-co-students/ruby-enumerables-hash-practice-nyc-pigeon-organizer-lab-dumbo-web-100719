f3afee92c56a64998d84005f157bd9b68e31376a
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-30 11:58:18 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,22 @@
 def nyc_pigeon_organizer(data)
+#create new hash
   Hash.new = sorted_hash
+#iterate through original hash
     data.each do |key, value|
-      if data.has_key?(:color) && data(:color).has_key? 
+#extract names (need help on this - how to just grab all bottom level values )
+      if data.has_value?
+# put names in new hash
         sorted_hash = key
-    sorted_hash << data[key]
     end
        end
-      end
+          end
   sorted_hash
 end
 
 
 # if structure.has_key?(:a) && structure[:a].has_key?(:b) then
 #   value = structure[:a][:b]
+
+
 
 
