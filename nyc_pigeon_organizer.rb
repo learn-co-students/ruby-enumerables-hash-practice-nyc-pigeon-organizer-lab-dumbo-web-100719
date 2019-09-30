d747271c683d23b2a4a758f9ffafcbb6dc193f5e
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-30 15:06:19 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -21,7 +21,7 @@ def nyc_pigeon_organizer(data)
     value.each do |new_value, names|
       names.each do |name|
        new_hash[name]= {}
-       new_hash[name]= value[new_value]
+       new_hash[name]= key[new_value]
         binding.pry
       end
     end
