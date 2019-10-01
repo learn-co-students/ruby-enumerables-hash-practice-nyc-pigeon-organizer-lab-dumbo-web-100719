c56b2d45a40f05953bcc015a781c344817493a7b
Muna Abdelrazeq <mma207@scarletmail.rutgers.edu>
2019-10-01 23:03:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  hash = {}
+  data.each do |general, specific|
+    specific.each do |attribute, names|
+      names.each do |name|
+        
+        if !hash[name]
+          hash[name] = {}
+        end
+
+        if !hash[name][general]
+          hash[name][general] = []
+        end
+
+        hash[name][general].push(attribute.to_s)
+
+      end
+    end
+  end
+  hash
+end 
+
+
