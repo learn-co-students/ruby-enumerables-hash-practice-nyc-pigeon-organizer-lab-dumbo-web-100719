ca771618132f153f3587f60f36035f0a5f8056f0
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-10-02 18:55:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,36 +1,29 @@
-require 'pry'
+
 def nyc_pigeon_organizer(pigeon_data)
   
 pigeon_data_by_name = {}
 
 pigeon_data.each do |key, hash_value|
  
-
+  
 hash_value.each do |another_key, another_hash_value|
+ 
 another_hash_value.each do |names|
+
  
 if !pigeon_data_by_name[names]
   pigeon_data_by_name[names] = {}
-  
 end
 
-if !pigeon_data_by_name[names][key] = [
-  another_key.to_s]
+if !pigeon_data_by_name[names][key]
+  pigeon_data_by_name[names][key]=[]
 end
 
-#if pigeon_data_by_name[names][key]
-#if ![key]
-#pigeon_data_by_name[names][key]+=[another_key.to_s]
+pigeon_data_by_name[names][key] << another_key.to_s
+end
+end
+end
 
-
- 
-end
-end
-end
-end
-  
-end
-binding.pry
 pigeon_data_by_name 
 
 end
